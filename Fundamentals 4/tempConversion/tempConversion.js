6c16dd5a3c2df59b5efd0d675f61c654b1bfb5e8
semilija <sulsemilija@gmail.com>
2021-06-19 23:19:16 +0300

    Fundamentals 4 solutions version1

--- Fundamentals 4/tempConversion/tempConversion.js
+++ Fundamentals 4/tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  return parseFloat(((fahrenheit - 32) * (5 / 9)).toFixed(1));
 
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return parseFloat(((celsius * (9 / 5)) + 32).toFixed(1));
 }
 
 module.exports = {
